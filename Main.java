cf786007ec79137d134751346f1d3c996ad24649
147Y1A0551 <31858397+147Y1A0551@users.noreply.github.com>
2017-09-13 13:07:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		 system.out.println("enter the full name");
+		system.out.println("enter the birthdate");
+		system.out.println("enter the average marks")
 		
-		//You may test that your code works find here
-		//Please check that your code works and has no 
-		//compilation problems before to submit
 	}
 
 }
